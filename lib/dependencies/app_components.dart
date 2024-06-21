ca6d13c005ab7de7cff885581f5c4c0a54c64a20
Kayky Barbosa <contatokaykybarbosa@gmail.com>
2024-06-21 20:11:28 -0300

    feat: inicializando o firebase

---
+++ lib/dependencies/app_components.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lojavirtualapp/firebase_options.dart';
+
+abstract class AppComponents {
+  static Future<void> initApp() async {
+    _initFirebase();
+  }
+}
+
+Future<void> _initFirebase() async => await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:lojavirtualapp/screens/base/base_screen.dart';
-import 'package:lojavirtualapp/utils/theme/colors/my_colors.dart';
-import 'package:lojavirtualapp/utils/theme/theme_data/app_bar_theme.dart';
+import 'package:lojavirtualapp/dependencies/app_components.dart';
+import 'package:lojavirtualapp/my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await AppComponents.initApp();
+
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Loja do Kbuloso',
-        theme: ThemeData(
-          useMaterial3: true,
-          primaryColor: MyColors.primary,
-          scaffoldBackgroundColor: MyColors.primary,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          appBarTheme: TAppBarTheme.light,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: BaseScreen(),
-      );
 }
