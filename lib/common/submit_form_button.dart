8bc9874e5670c3abbb2860a702e8129c987fb7dd
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-23 18:53:08 -0300

    feat: criando componente submitFormButton

---
+++ lib/common/submit_form_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SubmitFormButton extends StatelessWidget {
+  const SubmitFormButton({
+    super.key,
+    this.text = '',
+    this.isLoading = false,
+    this.onPressed,
+  });
+
+  final String text;
+  final bool isLoading;
+  final Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        onPressed: onPressed,
+        child: isLoading
+            ? const SizedBox(height: 20, width: 20, child: CircularProgressIndicator.adaptive())
+            : Text(
+                text,
+                style: const TextStyle(fontSize: 18),
+              ),
+      );
+}
