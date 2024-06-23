1def95ca459d3c25a22dd94e81a2e82feda0d2ff
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-23 18:56:23 -0300

    feat: implemetando registro de usuários

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,7 +1,10 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtualapp/models/user_model.dart';
+import 'package:lojavirtualapp/utils/firebase_errors.dart';
 
 class UserManager extends ChangeNotifier {
   UserManager() {
@@ -9,8 +12,20 @@ class UserManager extends ChangeNotifier {
   }
 
   final _auth = FirebaseAuth.instance;
+  final _store = FirebaseFirestore.instance;
+
   bool _loading = false;
-  User? _currentUser;
+  UserModel? _currentUser;
+
+  Future<void> _loadCurrentUser([User? firebaseUser]) async {
+    final user = firebaseUser ?? _auth.currentUser;
+
+    if (user != null) {
+      final docUser = await _store.collection('users').doc(user.uid).get();
+
+      currentUser = UserModel.fromMapDB(docUser.id, docUser.data() ?? {});
+    }
+  }
 
   Future<void> signIn({
     required String email,
@@ -26,9 +41,11 @@ class UserManager extends ChangeNotifier {
         password: password,
       );
 
-      currentUser = result.user!;
+      _loadCurrentUser(result.user);
 
       if (onSuccess != null) onSuccess();
+    } on FirebaseAuthException catch (e) {
+      if (onFail != null) onFail(FirebaseErrors.getError(e.code, 'E-mail ou senha inválido'));
     } catch (e) {
       if (onFail != null) onFail('E-mail ou senha inválido');
     }
@@ -36,21 +53,38 @@ class UserManager extends ChangeNotifier {
     loading = false;
   }
 
-  void _loadCurrentUser() {
-    final user = _auth.currentUser;
+  Future<void> signUp({
+    required UserModel user,
+    Function()? onSuccess,
+    Function(String error)? onFail,
+  }) async {
+    loading = true;
+    try {
+      final result = await _auth.createUserWithEmailAndPassword(email: user.email, password: user.password);
 
-    if (user != null) {
+      user = user.copyWith(id: result.user?.uid);
+
       currentUser = user;
+
+      await user.saveData();
+
+      if (onSuccess != null) onSuccess();
+    } on FirebaseAuthException catch (e) {
+      if (onFail != null) onFail(FirebaseErrors.getError(e.code));
+    } catch (e) {
+      if (onFail != null) onFail(e.toString());
     }
+
+    loading = false;
   }
 
   // G E T T E R S
   bool get isLoading => _loading;
 
-  User? get getCurrentUser => _currentUser;
+  UserModel? get getCurrentUser => _currentUser;
 
   // S E T T E R S
   set loading(bool value) => {_loading = value, notifyListeners()};
 
-  set currentUser(User user) => {_currentUser = user, notifyListeners(), log(user.uid)};
+  set currentUser(UserModel user) => {_currentUser = user, notifyListeners(), log('$user')};
 }
