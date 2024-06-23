b8c50a9fca77e5196abc6432301a52bc2b4a5b4d
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-23 18:52:18 -0300

    feat: adicionando ícones que será utilizado no app

---
+++ lib/utils/theme/icons/my_icons.dart
@@ -0,0 +1,7 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/material.dart';
+
+abstract class MyIcons {
+  static const IconData eyeOn = EvaIcons.eyeOutline;
+  static const IconData eyeOff = EvaIcons.eyeOffOutline;
+}
