b0d6ccdcfc85ac1d76fa5ce3ab1ee8ca5e2ec839
Kayky Barbosa <kaykyb629@gmail.com>
2024-06-23 20:15:14 -0300

    refactor: melhorando o drawer

--- lib/ui/common/custom_drawer/custom_drawer.dart
+++ lib/ui/common/custom_drawer/custom_drawer.dart
@@ -1,32 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtualapp/ui/common/custom_drawer/custom_drawer_header.dart';
 import 'package:lojavirtualapp/ui/common/custom_drawer/drawer_tile.dart';
+import 'package:lojavirtualapp/utils/theme/colors/my_colors.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) => Drawer(
-        child: ListView(
-          children: const [
-            DrawerTile(
-              page: 0,
-              title: 'Início',
-              icon: Icons.home,
+        child: Stack(
+          children: <Widget>[
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: <Color>[
+                  MyColors.gradient,
+                  MyColors.base100,
+                ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+              ),
             ),
-            DrawerTile(
-              page: 1,
-              title: 'Produtos',
-              icon: Icons.list,
-            ),
-            DrawerTile(
-              page: 2,
-              title: 'Meus pedidos',
-              icon: Icons.playlist_add_check,
-            ),
-            DrawerTile(
-              page: 3,
-              title: 'Lojas',
-              icon: Icons.location_on,
+            ListView(
+              children: <Widget>[
+                const CustomDrawerHeader(),
+                Divider(color: MyColors.base100.withAlpha(100)),
+                const DrawerTile(
+                  page: 0,
+                  title: 'Início',
+                  icon: Icons.home,
+                ),
+                const DrawerTile(
+                  page: 1,
+                  title: 'Produtos',
+                  icon: Icons.list,
+                ),
+                const DrawerTile(
+                  page: 2,
+                  title: 'Meus pedidos',
+                  icon: Icons.playlist_add_check,
+                ),
+                const DrawerTile(
+                  page: 3,
+                  title: 'Lojas',
+                  icon: Icons.location_on,
+                ),
+              ],
             ),
           ],
         ),
