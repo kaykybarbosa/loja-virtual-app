1f2b1590b397907e9cbf15434427ce25a7e070f9
Kayky Barbosa <contatokaykybarbosa@gmail.com>
2024-06-24 17:26:39 -0300

    refactor: adicionando widthe e height no submitFormButton

--- lib/ui/common/submit_form_button.dart
+++ lib/ui/common/submit_form_button.dart
@@ -5,21 +5,29 @@ class SubmitFormButton extends StatelessWidget {
     super.key,
     this.text = '',
     this.isLoading = false,
+    this.width,
+    this.height = 44,
     this.onPressed,
   });
 
   final String text;
   final bool isLoading;
+  final double? width;
+  final double? height;
   final Function()? onPressed;
 
   @override
-  Widget build(BuildContext context) => ElevatedButton(
-        onPressed: onPressed,
-        child: isLoading
-            ? const SizedBox(height: 20, width: 20, child: CircularProgressIndicator.adaptive())
-            : Text(
-                text,
-                style: const TextStyle(fontSize: 18),
-              ),
+  Widget build(BuildContext context) => SizedBox(
+        width: width,
+        height: height,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          child: isLoading
+              ? const SizedBox(height: 20, width: 20, child: CircularProgressIndicator.adaptive())
+              : Text(
+                  text,
+                  style: const TextStyle(fontSize: 18),
+                ),
+        ),
       );
 }
