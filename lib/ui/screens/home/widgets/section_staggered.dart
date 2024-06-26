93425bfaa381a04783e40ec4b1bc81488fd33425
Kayky Barbosa <contatokaykybarbosa@gmail.com>
2024-06-26 09:47:33 -0300

    refactor: finalizando implementação dos ítens em staggered

--- lib/ui/screens/home/widgets/section_staggered.dart
+++ lib/ui/screens/home/widgets/section_staggered.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:lojavirtualapp/domain/models/section_model.dart';
+import 'package:lojavirtualapp/ui/screens/home/widgets/item_tile.dart';
 import 'package:lojavirtualapp/ui/screens/home/widgets/section_header.dart';
 
 class SectionStaggered extends StatelessWidget {
@@ -8,15 +10,30 @@ class SectionStaggered extends StatelessWidget {
   final SectionModel section;
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SectionHeader(section: section),
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SectionHeader(section: section),
+            StaggeredGrid.count(
+              crossAxisCount: 4,
+              mainAxisSpacing: 4,
+              crossAxisSpacing: 4,
+              children: section.items.asMap().entries.map(
+                (entry) {
+                  final index = entry.key;
+                  final item = entry.value;
+
+                  return StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: index.isEven ? 2 : 1,
+                    child: ItemTile(image: item.image),
+                  );
+                },
+              ).toList(),
+            ),
+          ],
+        ),
+      );
 }
