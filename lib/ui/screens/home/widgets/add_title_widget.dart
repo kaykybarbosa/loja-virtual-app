8dbe85aed5388cdfb5411d5405b09f0d6626d5b8
Kayky Barbosa <kayky@abrem.net>
2024-08-23 17:57:15 -0300

    refactor: adicionando método onImageSelected em AddTitleWidget

--- lib/ui/screens/home/widgets/add_title_widget.dart
+++ lib/ui/screens/home/widgets/add_title_widget.dart
@@ -2,9 +2,13 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lojavirtualapp/domain/models/section_item_model.dart';
+import 'package:lojavirtualapp/domain/models/section_model.dart';
 import 'package:lojavirtualapp/ui/screens/products/sub_screens/widgets/image_source_sheet.dart';
 import 'package:lojavirtualapp/utils/theme/colors/my_colors.dart';
 import 'package:lojavirtualapp/utils/theme/icons/my_icons.dart';
+import 'package:provider/provider.dart';
 
 class AddTitleWidget extends StatelessWidget {
   const AddTitleWidget({super.key, this.onTap});
@@ -13,7 +17,13 @@ class AddTitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    void onImageSelected(File file) {}
+    final section = context.watch<SectionModel>();
+
+    void onImageSelected(File file) {
+      section.addItem(SectionItemModel(image: file));
+
+      context.pop();
+    }
 
     return GestureDetector(
       onTap: () {
