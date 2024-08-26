11abff8238acb89d73269eb57a448706eee750b5
Kayky Barbosa <kayky@abrem.net>
2024-08-26 09:33:15 -0300

    refactor: removendo logs

--- lib/ui/screens/home/widgets/section_list.dart
+++ lib/ui/screens/home/widgets/section_list.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:lojavirtualapp/data/managers/home_manager.dart';
@@ -9,38 +7,17 @@ import 'package:lojavirtualapp/ui/screens/home/widgets/item_tile.dart';
 import 'package:lojavirtualapp/ui/screens/home/widgets/section_header.dart';
 import 'package:provider/provider.dart';
 
-class SectionList extends StatefulWidget {
+class SectionList extends StatelessWidget {
   const SectionList(this.section, {super.key});
 
   final SectionModel section;
 
   @override
-  State<SectionList> createState() => _SectionListState();
-}
-
-class _SectionListState extends State<SectionList> {
-  @override
-  void initState() {
-    super.initState();
-
-    log('INIT', name: 'FIX');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    log('DISPOSED', name: 'FIX');
-  }
-
-  @override
   Widget build(BuildContext context) {
     final homeManager = context.watch<HomeManager>();
 
-    log(widget.section.toString(), name: 'FIX');
-
     return ChangeNotifierProvider.value(
-      value: widget.section,
+      value: section,
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Column(
